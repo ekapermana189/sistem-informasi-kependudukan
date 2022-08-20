95d4c990bb45cec512cf6de5e2439b8f93bc1459
Eka Permana <ekapermana1895@gmail.com>
2022-08-20 14:55:19 +0700

    add mendu

--- admin/mendu/data_mendu.php
+++ admin/mendu/data_mendu.php
@@ -1,7 +1,8 @@
 <div class="card card-info">
 	<div class="card-header">
 		<h3 class="card-title">
-			<i class="fa fa-table"></i> Data Kematian</h3>
+			<i class="fa fa-table"></i> Data Kematian
+		</h3>
 	</div>
 	<!-- /.card-header -->
 	<div class="card-body">
@@ -19,49 +20,47 @@
 						<th>Nama</th>
 						<th>Tanggal</th>
 						<th>Sebab</th>
-						<th>Aksi</th>
+
 					</tr>
 				</thead>
 				<tbody>
 
 					<?php
-              $no = 1;
-			  $sql = $koneksi->query("SELECT p.id_pend, p.nik, p.nama, m.tgl_mendu, m.sebab, m.id_mendu from 
+					$no = 1;
+					$sql = $koneksi->query("SELECT p.id_pend, p.nik, p.nama, m.tgl_mendu, m.sebab, m.id_mendu from 
 			  tb_mendu m inner join tb_pdd p on p.id_pend=m.id_pdd");
-              while ($data= $sql->fetch_assoc()) {
-            ?>
+					while ($data = $sql->fetch_assoc()) {
+					?>
 
-					<tr>
-						<td>
-							<?php echo $no++; ?>
-						</td>
-						<td>
-							<?php echo $data['nik']; ?>
-						</td>
-						<td>
-							<?php echo $data['nama']; ?>
-						</td>
-						<td>
-							<?php echo $data['tgl_mendu']; ?>
-						</td>
-						<td>
-							<?php echo $data['sebab']; ?>
-						</td>
-						<td>
-							<a href="?page=edit-mendu&kode=<?php echo $data['id_mendu']; ?>" title="Ubah"
-							 class="btn btn-success btn-sm">
-								<i class="fa fa-edit"></i>
-							</a>
-							<a href="?page=del-mendu&kode=<?php echo $data['id_pend']; ?>" onclick="return confirm('Apakah anda yakin hapus data ini ?')"
-							 title="Hapus" class="btn btn-danger btn-sm">
-								<i class="fa fa-trash"></i>
-								</>
-						</td>
-					</tr>
+						<tr>
+							<td>
+								<?php echo $no++; ?>
+							</td>
+							<td>
+								<?php echo $data['nik']; ?>
+							</td>
+							<td>
+								<?php echo $data['nama']; ?>
+							</td>
+							<td>
+								<?php echo $data['tgl_mendu']; ?>
+							</td>
+							<td>
+								<?php echo $data['sebab']; ?>
+							</td>
+							<td>
+								<a href="?page=edit-mendu&kode=<?php echo $data['id_mendu']; ?>" title="Ubah" class="btn btn-success btn-sm">
+									<i class="fa fa-edit"></i>
+								</a>
+								<a href="?page=del-mendu&kode=<?php echo $data['id_pend']; ?>" onclick="return confirm('Apakah anda yakin hapus data ini ?')" title="Hapus" class="btn btn-danger btn-sm">
+									<i class="fa fa-trash"></i>
+									</>
+							</td>
+						</tr>
 
 					<?php
-              }
-            ?>
+					}
+					?>
 				</tbody>
 				</tfoot>
 			</table>
