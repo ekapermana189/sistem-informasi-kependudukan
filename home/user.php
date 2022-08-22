396e2bfd48752edf2ceef88c9ee28795679159ee
Eka Permana <ekapermana1895@gmail.com>
2022-08-22 20:53:50 +0700

    welcome page user

---
+++ home/user.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger alert-dismissible">
+    <h4 class="mx-2 mt-2 text-lg"><i class="icon fa fa-info"></i> Welcome </h4>
+    <p class="-mt-1 ml-2 text-base">di Sistem Informasi Data Kependudukan</p>
+</div>
